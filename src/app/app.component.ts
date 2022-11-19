d53ee21b8da1794b14b6d542fb06b12c122a2cf0
kuno449 <kunoyoshiki@hotmail.com>
2022-11-19 08:58:28 +0100

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { QueryList, Component, ViewChildren } from '@angular/core';
+import { CdkDropList, CdkDragDrop } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'widget-dashboard';
+  dropped($event: CdkDragDrop<string>) {
+    console.log($event);
+  }
+
+  @ViewChildren(CdkDropList) dropsQuery!: QueryList<CdkDropList>;
+
+  drops!: CdkDropList[];
+
+  ngAfterViewInit() {
+    this.dropsQuery.changes.subscribe(() => {
+      this.drops = this.dropsQuery.toArray()
+    })
+    Promise.resolve().then(() => {
+      this.drops = this.dropsQuery.toArray();
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSelectModule} from '@angular/material/select';
+import { MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatButtonModule} from '@angular/material/button';
+import { MatRadioModule} from '@angular/material/radio';
+import { MatCheckboxModule} from '@angular/material/checkbox';
+import { MatCardModule} from '@angular/material/card';
+import { MatGridListModule} from '@angular/material/grid-list';
+import { MatMenuModule} from '@angular/material/menu';
+import { DragDropModule} from '@angular/cdk/drag-drop';
+import { LayoutModule } from '@angular/cdk/layout';
+import { DashboardComponent } from './dashboard/dashboard.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatDividerModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatCardModule,
+    DragDropModule,
+    MatGridListModule,
+    MatMenuModule,
+    DragDropModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
